2a80d0d785f517f28c23eedfed7842eae8fda0e3
Steve Flagg Jr <sflaggjr@gmail.com>
2015-05-27 18:08:06 -0400

    commit 4

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-$(document).ready(){
-  
-}
+$(document).ready(function(){
+  $('#')
+  $('#submit').click(function(evt){
+    evt.preventDefault();
+    var data = getData();
+    console.log(data);
+    outputData(data);
+  });
+});
+
+
+function getData()
+  {
+    var data = {};
+    $('.dataPoint').each(function(index, inputData){
+      data[inputData.id] = inputData.value;
+    });
+    return data;
+  };
+
+function outputData(data)
+  {
+    var inputArray = [
+      {'status': 'newStatus'},{'height': 'newHeight'}
+    ];
+    $.each(inputArray,function(index, field){
+      $.each(field, function(origin, destination){
+        console.log('Original Field:' + origin, "New Field:" + destination);
+      })
+    });
+  };
